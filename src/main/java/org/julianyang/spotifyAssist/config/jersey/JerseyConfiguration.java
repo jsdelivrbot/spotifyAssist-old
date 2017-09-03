48adece0a6dc295587007621b66b979263aead8d
Julian Yang <yang.julian@gmail.com>
2017-09-02 20:53:20 -0700

    clean up JerseyConfiguration

--- src/main/java/org/julianyang/spotifyAssist/config/jersey/JerseyConfiguration.java
+++ src/main/java/org/julianyang/spotifyAssist/config/jersey/JerseyConfiguration.java
@@ -1,9 +1,7 @@
 package org.julianyang.spotifyAssist.config.jersey;
 
-
 import com.google.inject.Injector;
 import org.glassfish.hk2.api.ServiceLocator;
-import org.glassfish.hk2.api.ServiceLocatorFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.mvc.freemarker.FreemarkerMvcFeature;
 import org.jvnet.hk2.guice.bridge.api.GuiceBridge;
@@ -25,11 +23,7 @@ public class JerseyConfiguration extends ResourceConfig {
 
 		GuiceBridge.getGuiceBridge().initializeGuiceBridge(serviceLocator);
 		GuiceIntoHK2Bridge guiceBridge = serviceLocator.getService(GuiceIntoHK2Bridge.class);
-		Injector injector = (Injector) servletContext.getAttribute(Injector.class.getName());
-		if (injector == null) {
-			log.info("Injector was null!");
-		}
-		guiceBridge.bridgeGuiceInjector(injector);
+		guiceBridge.bridgeGuiceInjector((Injector) servletContext.getAttribute(Injector.class.getName()));
 		log.info("Finish JerseyConfiguration");
 	}
 }
