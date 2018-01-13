b3a0d93290786dbedbd9ac3d89b231893e7e3798
Julian Yang <yang.julian@gmail.com>
2018-01-13 13:32:00 -0800

    Clean up spotify.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 const spotify = require('./spotify.js');
 
 express()
@@ -9,5 +9,6 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/login-spotify', (req, res) => res.redirect(spotify.authorizeURL))
-  .get('/spotify-callback', async (req, res) => spotify.processCallback(req, res))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get(
+    '/spotify-callback', async (req, res) => spotify.processCallback(req, res))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
--- spotify.js
+++ spotify.js
@@ -1,28 +1,32 @@
-var SpotifyWebApi = require('spotify-web-api-node');
-
-var scopes = ['user-read-private', 'user-read-email'],
-    //redirectUri = 'localhost:5000/spotify-callback',
-    redirectUri = 'ENV' in process.env
+let SpotifyWebApi = require('spotify-web-api-node');
+let scopes = ['user-read-private', 'user-read-email'];
+let redirectUri = 'ENV' in process.env
         ? 'http://localhost:5000/spotify-callback'
-        : 'https://spotify-assist.herokuapp.com/spotify-callback',
-    clientId = process.env.SPOTIFY_CLIENT_ID,
-    clientSecret = process.env.SPOTIFY_SECRET,
-    state = 'some-state-of-my-choice';
+        : 'https://spotify-assist.herokuapp.com/spotify-callback';
+let clientId = process.env.SPOTIFY_CLIENT_ID;
+let clientSecret = process.env.SPOTIFY_SECRET;
+let state = 'some-state-of-my-choice';
 
-// Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
-var spotifyApi = new SpotifyWebApi({
-  redirectUri : redirectUri,
-  clientId : clientId,
-  clientSecret : clientSecret
+// Setting credentials can be done in the wrapper's constructor, or using the
+// API object's setters.
+let spotifyApi = new SpotifyWebApi({
+  redirectUri: redirectUri,
+  clientId: clientId,
+  clientSecret: clientSecret,
 });
 
 // Create the authorization URL
-var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state, true);
+let authorizeURL = spotifyApi.createAuthorizeURL(scopes, state, true);
 
 exports.authorizeURL = authorizeURL;
 // https://accounts.spotify.com:443/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice
 console.log(authorizeURL);
 
+/**
+ * Fetches access token from Spotify API.
+ * @param {string} code: the authorization code to use
+ * @param {function} callback: callback when finishing retrieving token.
+ * */
 function retrieveToken(code, callback) {
   spotifyApi.authorizationCodeGrant(code)
     .then(function(data) {
@@ -41,8 +45,13 @@ function retrieveToken(code, callback) {
     });
 }
 
+/**
+ * Fetches "me" from SpotifyApi and renders the page in response.
+ * @param {http.ServerResponse} res: response to render.
+ * @param {string} code: authCode used to fetch token.
+ */
 async function fetchMeAndRender(res, code) {
-    var me = 'blagh';
+    let me = 'blagh';
     console.log('getAccessToken: ' + spotifyApi.getAccessToken());
     try {
       me = (await spotifyApi.getMe()).body;
@@ -50,18 +59,24 @@ async function fetchMeAndRender(res, code) {
       console.log('Something went wrong with getMe()!', err);
     }
     console.log('Some information about the authenticated user', me);
-    res.render('pages/spotify', {name: me.display_name, email: me.email, authCode: code});
+    res.render(
+      'pages/spotify',
+      {name: me.display_name, email: me.email, authCode: code});
   }
 
+/**
+ * Setup Spotify WebApi.
+ * @param {http.ServerRequest} req: server request
+ * @param {http.ServerResponse} res: server response
+ */
 async function processCallback(req, res) {
-  //var code = 'query' in req ? req.query.code : '';
-  //code = process.env.SPOTIFY_TOKEN;
-  // 'SPOTIFY_TOKEN' in process.env ? process.env.SPOTIFY_TOKEN :
+  // var code = 'query' in req ? req.query.code : '';
+  // code = process.env.SPOTIFY_TOKEN;
+  //  'SPOTIFY_TOKEN' in process.env ? process.env.SPOTIFY_TOKEN :
   var code = req.query.code;
   console.log(code);
   console.log('retrieving token')
   retrieveToken(code, () => fetchMeAndRender(res, code));
 }
 
-exports.processCallback = processCallback
-exports.retrieveToken = retrieveToken
+exports.processCallback = processCallback;
