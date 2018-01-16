5c2959fe90a9cab5043926adef5701a0631d4a05
Julian Yang <yang.julian@gmail.com>
2018-01-16 00:06:24 -0800

    enforce auth on /test

--- spotify2.js
+++ spotify2.js
@@ -1,9 +1,14 @@
-require('./authorization/authCode');
+const authCode = require('./authorization/authCode');
 
 function test(req, res) {
   let authorization = req.header('Authorization');
   console.log(authorization);
-  res.status(200).send('hi!');
+  let token = authorization.split(' ')[1];
+  if (authCode.verifyValidAccessToken(token)) {
+    res.status(200).send('hi!');
+  } else {
+    res.status(400).send('bad auth!');
+  }
 }
 
 exports.test = test;
