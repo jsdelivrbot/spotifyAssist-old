55dfa205835f7f03b691cae2659a78c6bfee9dfd
Julian Yang <yang.julian@gmail.com>
2018-01-15 22:24:40 -0800

    clean up authRouter.js

--- authorization/authRouter.js
+++ authorization/authRouter.js
@@ -1,7 +1,11 @@
 
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const {GoogleAuth} = require('google-auth-library');
+const {OAuth2Client} = require('google-auth-library');
+const oAuth2Client = new OAuth2Client(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  constants.GOOGLE_LOGIN_CALLBACK_URL,
+);
 const express = require('express');
 const {URL} = require('url');
 const constants = require('../constants.js');
@@ -20,74 +24,12 @@ router.get('/checkSpotify', checkSpotify);
 
 exports.router = router;
 
-// Use the GoogleStrategy within Passport.
-//   Strategies in Passport require a `verify` function, which accept
-//   credentials (in this case, an accessToken, refreshToken, and Google
-//   profile), and invoke a callback with a user object.
-passport.use(new GoogleStrategy(
-  {
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: constants.GOOGLE_LOGIN_CALLBACK_URL, // 'https://spotify-assist.herokuapp.com/auth/googleCallback',
-  },
-  async function(accessToken, refreshToken, profile, done) {
-    let user = await db.findOrCreateUser(profile.id);
-    console.log(user);
-    done(null, user);
-    /* .findOrCreate(
-      {googleId: profile.id},
-      function(err, user) {
-      return done(err, user);
-      }); */
-  }
-));
-
-// Tell passport how to serialize / deserialize user.
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
-
-/**
- * Handle sign-in.
- * @param {http.ServerRequest} req: req
- * @param {http.ServerResponse} res: response to send.
- */
-function signIn(req, res) {
-  console.log(constants.GOOGLE_LOGIN_CALLBACK_URL);
-  // redirect to have user sign in to Google.
-  passport.authenticate(
-    'google', {scope: ['https://www.googleapis.com/auth/userinfo.profile'], successRedirect: '/auth/checkSpotify',
-    failureRedirect: '/login'})(req, res);
-}
-
-/**
- * Handle google callback after user signs into google.
- * @param {http.ServerRequest} req: req
- * @param {http.ServerResponse} res: response to send.
- */
-function googleCallback(req, res) {
-  passport.authenticate('google', {}, () => res.redirect('/auth/checkSpotify'))(req, res);
-}
-
 async function signinPost(req, res) {
   // Setup Google token verifier
-  const {OAuth2Client} = require('google-auth-library');
-  const oAuth2Client = new OAuth2Client(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    constants.GOOGLE_LOGIN_CALLBACK_URL,
-  );
-
   let idtoken = req.body.idtoken;
   let state = req.body.state;
   let redirectUrl = req.body.redirectUrl;
   let clientId = req.body.clientId;
-
-
   // Verify the id_token, and access the claims.
   try {
     const ticket = await oAuth2Client.verifyIdToken({
