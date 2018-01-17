3ced9c44ae9ed0269f0a10a7524e855049d15f2a
Julian Yang <yang.julian@gmail.com>
2018-01-17 00:34:01 -0800

    try to dump request to /webhook

--- spotify2.js
+++ spotify2.js
@@ -1,16 +1,17 @@
 const authCode = require('./authorization/authCode');
 
 function test(req, res) {
-  let authorization = req.header('Authorization');
-  console.log(authorization);
-  console.log(`req header: ${req.header}`);
-  console.log(`req body: ${req.body}`);
-  let token = authorization.split(' ')[1];
-  if (authCode.verifyValidAccessToken(token)) {
-    res.status(200).send('hi!');
-  } else {
-    res.status(400).send('bad auth!');
-  }
+  // let authorization = req.header('Authorization');
+  // console.log(authorization);
+  console.log(`req header: ${req.header()}`);
+  console.log(`req body: ${JSON.stringify(req.body)}`);
+  res.status(200).send('hi!');
+  // let token = authorization.split(' ')[1];
+  // if (authCode.verifyValidAccessToken(token)) {
+  //   res.status(200).send('hi!');
+  // } else {
+  //   res.status(400).send('bad auth!');
+  // }
 }
 
 exports.test = test;
