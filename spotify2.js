9285ca6e02fe4f22636a9372e8dd7f8fbabed0b2
Julian Yang <yang.julian@gmail.com>
2018-01-17 00:52:49 -0800

    try to make dialogflow app handle request

--- spotify2.js
+++ spotify2.js
@@ -1,5 +1,15 @@
 const authCode = require('./authorization/authCode');
 const dialogFlow = require('actions-on-google').DialogflowApp;
+
+const WELCOME_INTENT = 'input.welcome';  // the action name from the Dialogflow intent
+
+function welcomeIntent (app) {
+  app.tell('Welcome to Spotify Assist, hooray!'); 
+}
+
+const actionMap = new Map();
+actionMap.set(WELCOME_INTENT, welcomeIntent);
+
 function test(req, res) {
   // let authorization = req.header('Authorization');
   // console.log(authorization);
@@ -7,6 +17,8 @@ function test(req, res) {
   console.log(`headers: ${JSON.stringify(req.headers)}`);
   console.log(`body: ${JSON.stringify(req.body)}`);
   const app = new dialogFlow({request: req, response: res});
+  app.handleRequest(actionMap);
+
   res.sendStatus(200);
   // let token = authorization.split(' ')[1];
   // if (authCode.verifyValidAccessToken(token)) {
